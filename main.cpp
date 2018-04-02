8b14367b576b7ecbecb40f600cd2206b83d66452
starshunter <petercheng0303@gmail.com>
2018-04-02 21:18:21 +0800

    add exception
    add exception to add() and print()

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 using namespace std;
 
 class Entity
@@ -7,7 +8,7 @@ class Entity
 protected:
     string id;
     bool isOn;
-    bool isSer; // means nothing if isOn == false
+    bool isSer;
     double lon;
     double lat;
 public:
@@ -74,15 +75,15 @@ public:
     // EntityArray(const EntityArray& ea);
     // operator=(const EntityArray& ea);
     ~EntityArray();
-    bool add(string id,bool isOn,bool isSer,double lon,double lat);
-    void print();
+    void add(string id,bool isOn,bool isSer,double lon,double lat) throw(overflow_error);
+    void print() const noexcept;
 };
 
 template <typename T>
 EntityArray<T>::EntityArray()
 {
     this->cnt=0;
-    this->capacity=20000;
+    this->capacity=1;
     this->entityPtr=new Entity*[this->capacity];
 }
 
@@ -95,75 +96,63 @@ EntityArray<T>::~EntityArray()
 }
 
 template <typename T>
-void EntityArray<T>::print()
+void EntityArray<T>::print() const noexcept
 {
     for(int i=0;i<this->cnt;i++)
         this->entityPtr[i]->print();
 }
 
 template <typename T>
-bool EntityArray<T>::add(string id,bool isOn,bool isSer,double lon,double lat)
+void EntityArray<T>::add(string id,bool isOn,bool isSer,double lon,double lat) throw(overflow_error)
 {
     if(this->cnt<this->capacity)
     {
         this->entityPtr[this->cnt]=new T(id,isOn,isSer,lon,lat);
         this->cnt++;
-        return true;
     }
     else
-        return false;
+        throw overflow_error("...\n");
 }
-
-/*
-class CarArray:public EntityArray<T>
-{
-private:
-public:
-    bool add(string id,bool isOn,bool isSer,double lon,double lat);
-};
-
-bool CarArray::add(string id,bool isOn,bool isSer,double lon,double lat)
-{
-    if(this->cnt<this->capacity)
-    {
-        this->entityPtr[this->cnt]=new Car(id,isOn,isSer,lon,lat);
-        this->cnt++;
-        return true;
-    }
-    else
-        return false;
-}
-
-class PassengerArray:public EntityArray
-{
-private:
-public:
-    bool add(string id,bool isOn,bool isSer,double lon,double lat);
-};
-
-bool PassengerArray::add(string id,bool isOn,bool isSer,double lon,double lat)
-{
-    if(this->cnt<this->capacity)
-    {
-        this->entityPtr[this->cnt]=new Passenger(id,isOn,isSer,lon,lat);
-        this->cnt++;
-        return true;
-    }
-    else
-        return false;
-}
-*/
 
 int main()
 {
     EntityArray<Car> ca;
-    ca.add("5HE-313",true,true,0,0);
-    ca.add("LPA-039",true,false,1,1);
+    try
+    {
+        ca.add("5HE-313",true,true,0,0);
+    }
+    catch(overflow_error e)
+    {
+        cout<<e.what();
+    }
+    try
+    {
+        ca.add("LPA-039",true,false,1,1);
+    }
+    catch(overflow_error e)
+    {
+        cout<<e.what();
+    }
     ca.print();
     
     EntityArray<Passenger> pa;
-    pa.add("B90705023",true,true,0,0);
-    pa.add("R94725008",true,false,1,1);
+    try
+    {
+        pa.add("B90705023",true,true,0,0);
+    }
+    catch(overflow_error e)
+    {
+        cout<<e.what();
+    }
+    try
+    {
+        pa.add("R94725008",true,false,1,1);
+    }
+    catch(overflow_error e)
+    {
+        cout<<e.what();
+    }
+    
     pa.print();
     
     return 0;
